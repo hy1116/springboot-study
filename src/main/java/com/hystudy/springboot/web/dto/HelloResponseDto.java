45a4840ea91901ab07340c8a6e895df15014daf4
HyunYuoungC <daiunii@naver.com>
2022-01-20 00:24:20 +0900

    ~73p 공부

--- src/main/java/com/hystudy/springboot/web/dto/HelloResponseDto.java
+++ src/main/java/com/hystudy/springboot/web/dto/HelloResponseDto.java
@@ -3,7 +3,7 @@ package com.hystudy.springboot.web.dto;
 import lombok.Getter;
 import lombok.RequiredArgsConstructor;
 
-@Getter // 선언된 모든 필드의 get method를 생성해줌
+@Getter // 선언된 모든 필드의 get 메소드를 생성해줌
 @RequiredArgsConstructor // 성성된 모든 final 필드의 생성자를 생성해줌
 public class HelloResponseDto {
     private final String name;
