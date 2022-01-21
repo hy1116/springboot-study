c0de605583ffbd7fb405b7257c9829c08b6886cc
Q219057 <daiunii@naver.com>
2022-01-21 18:05:08 +0900

    프로그래머스 키패트 누르기 문제 추가

--- src/main/java/com/hystudy/springboot/web/ProgrammersController.java
+++ src/main/java/com/hystudy/springboot/web/ProgrammersController.java
@@ -3,6 +3,7 @@ package com.hystudy.springboot.web;
 import java.util.*;
 
 public class ProgrammersController {
+	// Lv.1 신고 결과 받기
 	public int[] solution_RepostResult(String[] id_list, String[] report, int k) {
         int[] answer = new int[id_list.length];
         
@@ -29,23 +30,80 @@ public class ProgrammersController {
         return answer;
     }
 	
+	// LV.2 문자열 압축
 	public int solution_TextCompression(String s) {
-        int answer = s.length();
-        for(int i=1;i<=Math.floor(s.length()/2);i++){ 
-        	int cnt = 0; int eqcnt = 1;
-            int min = s.length();
-        	String prvword = s.substring(0,i);
-        	while(cnt < s.length()/i){
-        		String str = s.substring(i*++cnt,(i*(cnt+1) > s.length())?s.length():i*(cnt+1));
-        		if(prvword.equals(str)) eqcnt++;
-                else if(eqcnt > 1){
-                    min = min - ((eqcnt-1)*i)+((int)Math.log10(eqcnt)+1);
-        			eqcnt = 1;
+		int answer = s.length();
+        for(int i = 1; i <= Math.floor(s.length()/2); i++){  // 문자 단위
+        	int cnt = 0, eqcnt = 0; // 반복 횟수, 같은문자 카운트
+            int min = s.length(); // 압축 문자 길이
+        	String prevword = s.substring(0,i); // 이전 문자
+        	while(cnt++ < s.length()/i){ // 최대 문자열 반까지 자름
+        		// 현재 문자열 (마지막에 i개로 안잘릴수있으므로 마지막 카운트엔 length를 넣음)
+        		String str = s.substring( i*cnt, (i*(cnt+1) > s.length()) ? s.length() : i*(cnt+1) ); 
+        		if(prevword.equals(str)) eqcnt++; // 이전 문자와 같을 시 카운트 증가
+                else if(eqcnt > 0){ // 같은문자 카운트 끝났으면 줄어든 문자 수 계산
+                	// 줄어든 문자 수 계산 : (잘린 단위 X 반복된횟수-1 ) + 반복횟수 자릿 수(10일경우 2이므로)
+                    min = min - ( (eqcnt)*i) + ((int)Math.log10(eqcnt + 1) + 1 );
+        			eqcnt = 0; // 같은문자 카운트 초기화
                 }
-        		prvword = str;
+        		prevword = str; // 이전 문자에 현재문자 저장
         	}
-            if(min < answer) answer = min;
+            if(min < answer) answer = min; // i개로 잘랐을때의 갯수가 최솟값보다 작으면 갱신
         }
         return answer;
     }
+	
+	// LV.1 키패드 누르기
+	public String solution_keypadPress(int[] numbers, String hand) {
+        String answer = "";
+       
+        int[] left_current = {3,0}, right_current = {3,2}; // 왼손, 오른손 현재위치
+        for(int num : numbers) {
+        	switch(num) {
+        	case 1: case 4: case 7: // 1,4,7은 무조건 왼손
+        		answer+="L";
+        		left_current = clac_curr_location(num);
+        		break;
+        	case 3: case 6: case 9: // 3,6,9는 무조건 오른손
+        		answer+="R";
+        		right_current = clac_curr_location(num);
+        		break;
+        	case 2: case 5: case 8: case 0:
+        		int[] curr_loca = clac_curr_location(num);
+        		
+        		int left_distinct = 0;
+        		left_distinct += Math.abs(curr_loca[0] - left_current[0]);
+        		left_distinct += Math.abs(curr_loca[1] - left_current[1]);
+        		
+				int right_distinct = 0;
+				right_distinct += Math.abs(curr_loca[0] - right_current[0]);
+				right_distinct += Math.abs(curr_loca[1] - right_current[1]);
+        		
+				if(left_distinct < right_distinct || (left_distinct == right_distinct) &&"left".equals(hand)) {
+					answer += "L";
+        			left_current = clac_curr_location(num);
+				}
+        		else {
+        			answer += "R";
+					right_current = clac_curr_location(num);
+        		}
+        		break;
+        	}
+        }
+        
+        return answer;
+    }
+	
+	public int[] clac_curr_location(int num) { // 키패드 위치 계산
+		String[][] keypads = {{"1","2","3"},{"4","5","6"},{"7","8","9"},{"*","0","#"}}; // 2차원 키패드 배열
+        
+		int[] result = {0,0};
+		for(int i = 0; i < keypads.length; i++) {
+			if(Arrays.asList(keypads[i]).contains(String.valueOf(num))) {
+				result[0] = i;
+				result[1] = Arrays.asList(keypads[i]).indexOf(String.valueOf(num));
+			}
+		}
+		return result;
+	}
 }
--- src/test/java/com/hystudy/springboot/web/ProgrammersControllerTest.java
+++ src/test/java/com/hystudy/springboot/web/ProgrammersControllerTest.java
@@ -6,7 +6,7 @@ import org.junit.Test;
 
 public class ProgrammersControllerTest {
 	@Test
-	public void solution_RepostResult_test() {
+	public void solution_RepostResult_test() { // 신고 결과 받기 
 		ProgrammersController pc = new ProgrammersController();
 		
 		assertThat(pc.solution_RepostResult(
@@ -22,13 +22,22 @@ public class ProgrammersControllerTest {
 	}
 	
 	@Test
-	public void solution_TextCompression() {
+	public void solution_TextCompression_test() { // 문자열 압축
 		ProgrammersController pc = new ProgrammersController();
-
+		
 		assertThat(pc.solution_TextCompression("aabbaccc")).isEqualTo(7);
 		assertThat(pc.solution_TextCompression("ababcdcdababcdcd")).isEqualTo(9);
 		assertThat(pc.solution_TextCompression("abcabcdede")).isEqualTo(8);
 		assertThat(pc.solution_TextCompression("abcabcabcabcdededededede")).isEqualTo(14);
 		assertThat(pc.solution_TextCompression("xababcdcdababcdcd")).isEqualTo(17);
+	}
+	
+	@Test
+	public void solution_keypadPress_test() { // 문자열 압축
+		ProgrammersController pc = new ProgrammersController();
+		
+		assertThat(pc.solution_keypadPress(new int[] {1, 3, 4, 5, 8, 2, 1, 4, 5, 9, 5},"right")).isEqualTo("LRLLLRLLRRL");
+		assertThat(pc.solution_keypadPress(new int[] {7, 0, 8, 2, 8, 3, 1, 5, 7, 6, 2},"left")).isEqualTo("LRLLRRLLLRR");
+		assertThat(pc.solution_keypadPress(new int[] {1, 2, 3, 4, 5, 6, 7, 8, 9, 0},"right")).isEqualTo("LLRLLRLLRL");
 	}
 }
