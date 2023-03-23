d2b4be1cb5341130c0b5e9e1c01ee7d8566c5856
Q219057 <daiunii@naver.com>
2023-03-23 18:02:51 +0900

    beakjoon TEST 수정중

--- src/main/java/com/hystudy/springboot/web/BeakjoonController.java
+++ src/main/java/com/hystudy/springboot/web/BeakjoonController.java
@@ -1,48 +1,66 @@
 package com.hystudy.springboot.web;
 
-import java.io.BufferedReader;
-import java.io.InputStreamReader;
-import java.util.Arrays;
-import java.util.Comparator;
-import java.util.Scanner;
+import java.io.*;
+import java.util.*;
+import java.util.stream.*;
 
+/* 백준 제출방법
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+        while(st.countTokens()!=0)
+            bw.write(st.nextToken()+", ");
+
+        bw.flush();
+}
+*/
 public class BeakjoonController {
     // 숌 코드 https://www.acmicpc.net/problem/1651
-    public static int solution1(String[] arr){
-        /* 백준 제출방법
-        public class Main {
-            public static void main(String[] args) throws IOException {
-                BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-                BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
-                StringTokenizer st = new StringTokenizer(br.readLine());
-
-                while(st.countTokens()!=0)
-                    bw.write(st.nextToken()+", ");
-
-                bw.flush();
-
-        }
-        * */
+    public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        //for (br.lines())
-        //br.readLine();
+        String lines = br.readLine();
+        int answer = -1;
+        /*
+        Stream<String> params = lines;
+
         int answer = -1;
         // -1이 포함된 경우 무조건 0
-        if(Arrays.stream(arr).anyMatch(a->"-1".equals(a))) return 0;
-        // 길이 2이하인 경우 무조건 -1
-        if(arr.length<3) return -1;
+
+        if(params.anyMatch("-1"::equals)) answer=0;
+        // 길이 3이하인 경우 무조건 -1
+        if(params.count() <= 3) answer = -1;
         // 길이 순 정렬
-        Arrays.sort(arr,Comparator.comparing(String::length));
+
+        List<String> list = params.sorted(Comparator.comparing(String::length)).collect(Collectors.toList());
 
         int cnt = 0;
-        for(int i=2;i<arr.length;i++){
-            String s = arr[i];
-            if(s.equals(arr[i-1])) cnt ++;
-            if(s.contains(arr[i-1])) cnt ++;
-            if(s.contains(arr[i-1])) cnt ++;
-            if(cnt>=3) return s.length();
+        for(int i=2;i<list.size()-1;i++){
+            String s = list.get(i);
+            if(s.equals(list.get(i-1))) cnt ++;
+            if(s.contains(list.get(i-1))) cnt ++;
+            if(s.contains(list.get(i-1))) cnt ++;
+            if(cnt >= 3) answer = s.length();
         }
+        */
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        bw.write(String.valueOf(answer));
+        bw.flush();
 
-        return answer;
+        br.close();
+        bw.close();
+    }
+
+    public Stream<String> getParamsLines(){
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        Stream<String> lines = br.lines();
+        return lines;
+    }
+
+    public void submitAnswer(String answer) throws IOException {
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        bw.write(answer);
+        bw.flush();
     }
 }
--- src/test/java/com/hystudy/springboot/web/BeakjoonControllerTest.java
+++ src/test/java/com/hystudy/springboot/web/BeakjoonControllerTest.java
@@ -3,13 +3,29 @@ package com.hystudy.springboot.web;
 import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.Test;
 
+import java.io.*;
+import java.util.stream.Stream;
+
 public class BeakjoonControllerTest {
     @Test
-    public void testSolution1(){
-        Assertions.assertThat(BeakjoonController.solution1(new String[]{"1","1010","01","10101"})).isEqualTo(5);
-        Assertions.assertThat(BeakjoonController.solution1(new String[]{"0","11","11","11"})).isEqualTo(2);
-        Assertions.assertThat(BeakjoonController.solution1(new String[]{"0000","001","01001","01010","01011"})).isEqualTo(-1);
-        Assertions.assertThat(BeakjoonController.solution1(new String[]{"1","10","00"})).isEqualTo(-1);
+    public void testSolution1() throws IOException {
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        bw.write("4");
+        bw.write("1");
+        bw.write("1010");
+        bw.write("10");
+        bw.write("10101");
+        bw.flush();
+
+        BeakjoonController bc = new BeakjoonController();
+
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        String answer = "";
+        if(br.read() > -1){
+            answer = br.readLine();
+        }
+
+        Assertions.assertThat(answer).isEqualTo("5");
     }
 
 }
